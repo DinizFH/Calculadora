bdfddfabd23f595d0130d309887bd3a243ab7dce
João Antonio Diniz Filho <joaoantoniodinizfilho@gmail.com>
2021-05-23 10:20:22 -0400

    Create IMC.cpp

---
+++ C++/Projetos/IMC.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    string nome;
+    int idade;
+    char sexo;
+    double peso;
+
+    cout << "=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=\n";
+    cout << "IMC - Indice de massa corporal";
+    cout << "=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=\n";
+    cout<< "=-=-=-= CALCULADORA DE IMC =-=-=-=\n";
+    cout << "=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=\n";
+
+
+
+    return 0;
+}
