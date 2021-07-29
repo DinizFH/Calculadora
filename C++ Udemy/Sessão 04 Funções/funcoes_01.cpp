7344e885bd28a4834d7795b57992f606e95ad8b3
João Antonio Diniz Filho <joaoantoniodinizfilho@gmail.com>
2021-07-29 19:15:06 -0400

    Aulas

---
+++ C++ Udemy/Sessão 04 Funções/funcoes_01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void hello (){
+    cout << "Ola!!!\n";
+}
+
+void hello (string name){
+    cout << "Ola " << name << endl;
+}
+
+
+int main(){
+
+
+    return 0;
+}
